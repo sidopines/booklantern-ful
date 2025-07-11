71a10e89f37659c70af7187c90b473d56b308091
sidopines <sid.opines@gmail.com>
2025-07-11 09:48:28 +0400

    Remove dotenv for Render deployment

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Load environment variables
-require('dotenv').config();
 
 const adminEmail = process.env.ADMIN_EMAIL || 'admin@example.com';
 const adminPassword = process.env.ADMIN_PASS || 'admin123';
