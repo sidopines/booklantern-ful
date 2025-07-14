fd616be02542c68904e2389b7cdebe20325788d1
sidopines <sid.opines@gmail.com>
2025-07-14 20:42:07 +0400

    Update Video.js

--- models/Video.js
+++ models/Video.js
@@ -1,31 +1,13 @@
+// models/Video.js
 const mongoose = require('mongoose');
 
 const videoSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  genre: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Genre',
-    required: true,
-  },
-  youtubeUrl: {
-    type: String,
-    required: true,
-  },
-  thumbnail: {
-    type: String,
-    required: false,
-  },
-  description: {
-    type: String,
-    required: false,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  title: { type: String, required: true },
+  genre: { type: mongoose.Schema.Types.ObjectId, ref: 'Genre', required: true },
+  youtubeUrl: { type: String, required: true },
+  thumbnail: { type: String },
+  description: { type: String },
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Video', videoSchema);
