e9ff60767efb6ae7745647038c07535c763a8280
sidopines <sid.opines@gmail.com>
2025-07-15 16:32:27 +0400

    Create Favorite.js

---
+++ models/models/Favorite.js
@@ -0,0 +1,22 @@
+// models/Favorite.js
+
+const mongoose = require('mongoose');
+
+const favoriteSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+    required: true
+  },
+  addedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Favorite', favoriteSchema);
