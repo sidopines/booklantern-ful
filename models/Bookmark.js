c17b15fa97fa03da3b321b7e783096ccbd30b223
sidopines <sid.opines@gmail.com>
2025-07-15 21:53:22 +0400

    Create Bookmark.js

---
+++ models/Bookmark.js
@@ -0,0 +1,11 @@
+// models/Bookmark.js
+
+const mongoose = require('mongoose');
+
+const bookmarkSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
+  currentPage: { type: Number, default: 1 }
+});
+
+module.exports = mongoose.model('Bookmark', bookmarkSchema);
