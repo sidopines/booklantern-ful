1647d4b93e7e545e23fc1090c194d0fae81a3390
sidopines <sid.opines@gmail.com>
2025-07-23 21:58:01 +0400

    Update Bookmark.js

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,11 +1,35 @@
 // models/Bookmark.js
-
 const mongoose = require('mongoose');
 
-const bookmarkSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book', required: true },
-  currentPage: { type: Number, default: 1 }
+const bookmarkSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+
+    // For local DB books
+    book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
+
+    // For external (Archive.org) items
+    archiveId: { type: String, trim: true },
+
+    currentPage: { type: Number, default: 1 }
+  },
+  { timestamps: true }
+);
+
+bookmarkSchema.pre('validate', function (next) {
+  if (!this.book && !this.archiveId) {
+    return next(new Error('Either "book" or "archiveId" must be provided.'));
+  }
+  next();
 });
+
+bookmarkSchema.index(
+  { user: 1, book: 1 },
+  { unique: true, partialFilterExpression: { book: { $exists: true } } }
+);
+bookmarkSchema.index(
+  { user: 1, archiveId: 1 },
+  { unique: true, partialFilterExpression: { archiveId: { $exists: true } } }
+);
 
 module.exports = mongoose.model('Bookmark', bookmarkSchema);
